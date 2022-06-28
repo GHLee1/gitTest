594fe77f9a3b366d2ac8fc27c9424e9d131208eb
GHLee1 <wed8337@gmail.com>
2022-06-28 17:32:55 +0900

    second commit

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gittest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button2.setOnClickListener {
+            binding.text1.text = "text"
+        }
     }
     var i = 0
 }
